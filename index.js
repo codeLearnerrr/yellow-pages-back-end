8593e4716941bf57e6a6be5efb7412986f10ddc7
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-11-05 21:02:50 -0300

    tested basic queries

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const { Pool } = require('pg');
 
 const pool = new Pool();
 
-pool.query('SELECT * FROM people', (err, res) => {
-    console.log(err, res)
+let tables = ['people','category'];
+const query1 = ` SELECT * FROM ${tables[0]}`;
+const query2 = ` SELECT person_id, fname, lname, contact, category  FROM ${tables[0]}
+INNER JOIN ${tables[1]} ON category.category_id = people.category_id
+ORDER BY fname;`;
+
+pool.query(query2, (err, res) => {
+    console.log(err, res.rows)
     pool.end()
 })
