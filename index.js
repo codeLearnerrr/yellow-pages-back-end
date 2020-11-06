b26e2aa1bf27bb27f4996aabbc166ab32e121d1f
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-11-06 19:22:13 -0300

    added route and ajax request to db

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-
 const pool = new Pool();
 
-let tables = ['people','category'];
-const query1 = ` SELECT * FROM ${tables[0]}`;
-const query2 = ` SELECT person_id, fname, lname, contact, category  FROM ${tables[0]}
-INNER JOIN ${tables[1]} ON category.category_id = people.category_id
-ORDER BY fname;`;
+const fetch = require('node-fetch');
 
-pool.query(query2, (err, res) => {
-    console.log(err, res.rows)
-    pool.end()
-})
+const express = require('express');
+const app = express();
+const port = 5000;
+
+let tables = ['people', 'category'];
+// const query1 = ` SELECT * FROM ${tables[0]}`;
+const query2 = ` SELECT person_id, fname, lname, contact, category  FROM ${tables[0]}
+ INNER JOIN ${tables[1]} ON category.category_id = people.category_id
+ ORDER BY fname;`;
+
+fetch('http://localhost:5000/getPeopleJSON')
+    .then(res => res.json())
+    .then(body => console.log(body));
+
+
+app.get('/getAllPeopleJSON', async (req, res) => {
+    try {
+        console.log('try triggered');
+        const jsonResult = await pool.query(query2);
+        res.json(jsonResult.rows);
+    } catch (e) {
+        console.log('catch triggered', e);
+    }
+});
+app.listen(port, () => (console.log(`Example app listening at http://localhost:${port}`)))
