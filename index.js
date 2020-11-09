bf2ca110c5b95924aaa9184e0dd9fd1fda20a818
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-11-08 21:21:14 -0300

    added test route and cors

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const pool = new Pool();
 
 const fetch = require('node-fetch');
 
+
 const express = require('express');
 const app = express();
+
+const cors = require('cors');
+app.use(cors());
 const port = 5000;
 
 let tables = ['people', 'category'];
@@ -13,11 +17,6 @@ const query1 = ` SELECT * FROM ${tables[0]}`;
 const query2 = ` SELECT person_id, fname, lname, contact, category  FROM ${tables[0]}
  INNER JOIN ${tables[1]} ON category.category_id = people.category_id
  ORDER BY fname;`;
-
-fetch('http://localhost:5000/getAllPeopleJSON')
-    .then(res => res.json())
-    .then(body => console.log(body));
-
 
 app.get('/getAllPeopleJSON', async (req, res) => {
     try {
@@ -27,5 +26,10 @@ app.get('/getAllPeopleJSON', async (req, res) => {
     } catch (e) {
         console.log('catch triggered', e);
     }
+});
+const testArray=['hello 1','hello 2','hello 3']
+app.get('/getTestRoute', (req, res) => {
+    console.log('try from test route triggered');
+    res.send(testArray);
 });
 app.listen(port, () => (console.log(`Example app listening at http://localhost:${port}`)))
